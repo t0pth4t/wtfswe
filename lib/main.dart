b74527579c452bc6b52ca4f29cfe380ede3df081
t0pth4t <t0pth4t@gmail.com>
2018-08-01 18:47:37 -0500

    working with hardcoded json

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,22 @@ import 'package:location/location.dart';
 //
 Future<Restaurant> fetchRestaurant(lat, long) async {
   
-  final response = await http.get("https://developers.zomato.com/api/v2.1/cities?lat=$lat&lon=$long", headers: {'user-key': ''});
-
-  if(response.statusCode == 200){
-    final cities = parseCities(response.body);
-    return Restaurant.fromJson(json.decode(response.body));
-  } 
-
-    throw Exception('Failed to load');
+  final response = '{ "location": { "entity_type": "subzone", "entity_id": 123532, "title": "Fair Park", "latitude": "43.0160031766", "longitude": "-88.0107514723", "city_id": 1267, "city_name": "Milwaukee", "country_id": 216, "country_name": "United States" }, "popularity": { "popularity": "2.31", "nightlife_index": "1.80", "nearby_res": ["17176185", "17180021"], "top_cuisines": ["American", "Pizza", "Fast Food", "Bar Food", "Sandwich"], "popularity_res": "100", "nightlife_res": "10", "subzone": "Fair Park", "subzone_id": 123532, "city": "Milwaukee" }, "link": "https://www.zomato.com/milwaukee/fair-park-restaurants", "nearby_restaurants": [ { "restaurant": { "R": { "res_id": 17180021 }, "apikey": "", "id": "17180021", "name": "Monkey\'s Sports Bar & Grill", "url": "https://www.zomato.com/milwaukee/monkeys-sports-bar-grill-west-allis?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1", "location": { "address": "8028 West National Avenue, West Allis 53214", "locality": "Fair Park", "city": "Milwaukee", "city_id": 1267, "latitude": "43.0110420000", "longitude": "-88.0127200000", "zipcode": "53214", "country_id": 216, "locality_verbose": "Fair Park, Milwaukee" }, "switch_to_order_menu": 0, "cuisines": "Bar Food, Sandwich", "average_cost_for_two": 25, "price_range": 2, "currency": "dollar", "offers": [], "opentable_support": 0, "is_zomato_book_res": 0, "mezzo_provider": "OTHER", "is_book_form_web_view": 0, "book_form_web_view_url": "", "book_again_url": "", "thumb": "", "user_rating": { "aggregate_rating": "2.7", "rating_text": "Average", "rating_color": "FFBA00", "votes": "10" }, "photos_url": "https://www.zomato.com/milwaukee/monkeys-sports-bar-grill-west-allis/photos?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1#tabtop", "menu_url": "https://www.zomato.com/milwaukee/monkeys-sports-bar-grill-west-allis/menu?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1&openSwipeBox=menu&showMinimal=1#tabtop", "featured_image": "", "has_online_delivery": 0, "is_delivering_now": 0, "include_bogo_offers": true, "deeplink": "zomato://restaurant/17180021", "is_table_reservation_supported": 0, "has_table_booking": 0, "events_url": "https://www.zomato.com/milwaukee/monkeys-sports-bar-grill-west-allis/events#tabtop?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1" } }] } ';
   
+  // if(response.statusCode == 200){
+    // final restaurants = parseRestaurants(response.body);
+    final restaurants = parseRestaurants(response);
+    return restaurants[0];
+  // } 
+
+    // throw Exception('Failed to load');
+  
+}
+
+List<Restaurant> parseRestaurants(String responseBody){
+  final decoded = json.decode(responseBody);
+  // final parsed = decoded.cast<Map<String, dynamic>>();
+  return decoded['nearby_restaurants'].map<Restaurant>((json) => Restaurant.fromJson(json['restaurant'])).toList();
 }
 
 List<City> parseCities(String responseBody){
@@ -57,7 +64,7 @@ class Restaurant{
   Restaurant ({this.restaurantName});
 
   factory Restaurant.fromJson(Map<String, dynamic> json){
-    return Restaurant(restaurantName: json['title']);
+    return Restaurant(restaurantName: json['name']);
   }
 }
 
@@ -188,7 +195,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'How about?',
             ),
             FutureBuilder<Restaurant>(
-              future: fetchRestaurant(),
+              future: fetchRestaurant(_currentLocation == null ? "0" : _currentLocation["latitude"],_currentLocation == null ? "0" : _currentLocation["longitude"]),
               builder: (context, snapshot){
                 if(snapshot.hasData){
                   return Text(snapshot.data.restaurantName);
